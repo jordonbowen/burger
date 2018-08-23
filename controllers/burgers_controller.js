b0d671ee7bc7f874b720db73e1b50d707469af3e
jordonbowen <jordon_bowen@yahoo.com>
2018-08-23 16:24:43 -0400

    duh

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,44 +1,35 @@
-var express = require("express");
-
+var express = require('express');
 var router = express.Router();
+var burgers = require('../models/burger.js');
 
-
-var burger = require("../models/burger.js");
-
-
-router.get("/", function(req, res) {
-  burger.selectAll(function(data) {
-    var hbsObject = {
-      burgers: data
-    };
-    console.log(hbsObject);
-    res.render("index", hbsObject);
-  });
+router.get('/', function(req, res){
+	res.redirect('/burgers')
 });
 
+router.get('/burgers', function(req, res){
+	burgers.all(function(data){
+		var hbsObject = {burgers: data};
 
-router.post("/", function(req, res) {
-  burger.insertOne([
-    "burger_name", "devoured"
-  ], [
-    req.body.burger_name, req.body.devoured
-  ], function() {
-    res.redirect("/");
-  });
+		console.log(hbsObject);
+
+		res.render('index', hbsObject);
+	});
 });
 
-
-router.put("/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
-
-  console.log("condition", condition);
-
-  burger.updateOne({
-    devoured: req.body.devoured
-  }, condition, function() {
-    res.redirect("/");
-  });
+router.post('/burgers/create', function(req, res){
+	burgers.create(['burger_name'], [req.body.b_name], function(data){
+		res.redirect('/burgers')
+	});
 });
 
+router.put('/burgers/update/:id', function(req, res){
+	var condition = 'id = ' + req.params.id;
+
+	console.log('condition ', condition);
+
+	burgers.update({'devoured': req.body.devoured}, condition, function(data){
+		res.redirect('/burgers');
+	});
+});
 
 module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -1,24 +1,21 @@
+var orm = require('../config/orm.js');
 
-var orm = require("../config/orm.js");
-
-var burger = {
-  selectAll: function(cb) {
-    orm.selectAll("burgers", function(res) {
-      cb(res);
-    });
-  },
- 
-  insertOne: function(cols, vals, cb) {
-    orm.insertOne("burgers", cols, vals, function(res) {
-      cb(res);
-    });
-  },
-  updateOne: function(objColVals, condition, cb) {
-    orm.updateOne("burgers", objColVals, condition, function(res) {
-      cb(res);
-    });
-  }
+var burgers = {
+	all: function(cb){
+		orm.all('burgers', function(res){
+			cb(res);
+		});
+	},
+	create: function(col, vals, cb){
+		orm.create('burgers', col, vals, function(res){
+			cb(res);
+		});
+	},
+	update: function(objColVals, condition, cb){
+		orm.update('burgers', objColVals, condition, function(res){
+			cb(res);
+		});
+	}
 };
 
-
-module.exports = burger;
+module.exports = burgers;
