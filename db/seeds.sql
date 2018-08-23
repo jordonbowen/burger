aa85fd921bc7dc11cad17f866974c2ce63cb3e51
jordonbowen <jordon_bowen@yahoo.com>
2018-08-23 16:06:04 -0400

    did the thing

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon Burger with Cheese');
+INSERT INTO burgers (burger_name) VALUES ('Turkey Burger');
+INSERT INTO burgers (burger_name) VALUES ('Fish Sandwich with Cheese');
+INSERT INTO burgers (burger_name) VALUES ('Regular Hamburger');
