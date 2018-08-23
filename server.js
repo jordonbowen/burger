8bea6612cdfc449a935abaf6b51c44d82e8b327b
jordonbowen <jordon_bowen@yahoo.com>
2018-08-23 16:36:54 -0400

    changes

--- server.js
+++ server.js
@@ -4,18 +4,21 @@ var methodOverride = require('method-override')
 
 var app = express();
 
-//Serve static content for the app from the "public" directory in the application directory.
+
 app.use(express.static(process.cwd() + '/public'));
 
 app.use(bodyParser.urlencoded({
 	extended: false
 }))
-// override with POST having ?_method=DELETE
+
 app.use(methodOverride('_method'))
+
 var exphbs = require('express-handlebars');
+
 app.engine('handlebars', exphbs({
     defaultLayout: 'main',
 }));
+
 app.set('view engine', 'handlebars');
 
 var routes = require('./controllers/burgers_controller.js');
